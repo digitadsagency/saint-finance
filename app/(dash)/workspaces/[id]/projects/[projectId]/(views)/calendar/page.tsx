fc7caaa05556cf72b610f5db23570457f1b60496
digitadsagency <alvaro@digitads.com>
2025-11-10 10:54:36 -0600

    Fix: Change getPriorityColor case 'medium' to 'med'

--- app/(dash)/workspaces/[id]/projects/[projectId]/(views)/calendar/page.tsx
+++ app/(dash)/workspaces/[id]/projects/[projectId]/(views)/calendar/page.tsx
@@ -135,7 +135,7 @@ export default function ProjectCalendarPage({
     switch (priority) {
       case 'urgent': return 'bg-red-100 text-red-800 border-red-200'
       case 'high': return 'bg-orange-100 text-orange-800 border-orange-200'
-      case 'medium': return 'bg-yellow-100 text-yellow-800 border-yellow-200'
+      case 'med': return 'bg-yellow-100 text-yellow-800 border-yellow-200'
       case 'low': return 'bg-blue-100 text-blue-800 border-blue-200'
       default: return 'bg-gray-100 text-gray-800 border-gray-200'
     }
